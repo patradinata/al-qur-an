b1d28c5cb88799c99a83fb28dcce49247e1d75c9
patra dinata <patradinata08@gmail.com>
2025-02-23 02:41:12 +0700

    caches

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,33 @@
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("al-quran-cache-v1").then((cache) => {
-      return cache.addAll(["/", "/index.html", "/logo/logo-192x.png", "/logo/logo-256x.png", "/logo/logo-384x.png", "/logo/logo-512x.png"]);
-    })
+    caches
+      .open("al-quran-cache-v1")
+      .then((cache) => {
+        try {
+          return cache.addAll(["/", "/index.html", "/logo/logo-192x.png", "/logo/logo-256x.png", "/logo/logo-384x.png", "/logo/logo-512x.png"]);
+        } catch (error) {
+          console.error("Gagal menambahkan cache", error);
+        }
+      })
+      .catch((error) => {
+        console.error("Gagal membuka cache", error);
+      })
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    cache
+      .match(event.request)
+      .then((response) => {
+        if (response && response.ok) {
+          return response;
+        } else {
+          return fetch(event.request);
+        }
+      })
+      .catch((error) => {
+        console.error("Gagal mengambil response", error);
+      })
   );
 });
